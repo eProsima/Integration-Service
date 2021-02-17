f7206e37742eb7621e64b80e41aeada518ccf07c
laura-eprosima <laura@eprosima.com>
2021-02-17 09:31:40 +0000

    Fix warnings and tests
    Signed-off-by: LauraMartin <laura@eprosima.com>
    Signed-off-by: laura-eprosima <laura@eprosima.com>

--- packages/websocket/src/Server.cpp
+++ packages/websocket/src/Server.cpp
@@ -45,40 +45,42 @@ static std::string find_websocket_config_file(
         const std::string& config_key,
         const std::string& explanation)
 {
-  const soss::Search search = soss::Search(WebsocketMiddlewareName)
-      .relative_to_config()
-      .relative_to_home();
+    const soss::Search search = soss::Search(WebsocketMiddlewareName)
+            .relative_to_config()
+            .relative_to_home();
 
-  const YAML::Node node = configuration[config_key];
-  if(!node)
-  {
-   std::cerr << "[soss::websocket::Server] websocket_server is missing a "
-             << "value for the required parameter [" << config_key
-             << "] " << explanation << std::endl;
-   return {};
-  }
-  
-  std::vector<std::string> checked_paths;
+    const YAML::Node node = configuration[config_key];
+    if (!node)
+    {
+        std::cerr << "[soss::websocket::Server] websocket_server is missing a "
+                  << "value for the required parameter [" << config_key
+                  << "] " << explanation << std::endl;
+        return {};
+    }
 
-  const std::string& parameter = node.as<std::string>();
-  const std::string& result = search.find_file(parameter, "", &checked_paths);
-  if(result.empty())
-  {
-    std::string err = std::string()
-        + "[soss::websocket::Server] websocket_server failed to find the "
-        + "specified file for the [" + config_key + "] parameter: [" +parameter
-        + "]. Checked the following paths:\n";
-    for(const std::string& checked_path : checked_paths)
-      err += " -- " + checked_path + "\n";
-    std::cerr << err << std::endl;
-  }
-  else
-  {
-    std::cout << "[soss::websocket::Server] For [" << config_key << "] using ["
-              << result << "]" << std::endl;
-  }
+    std::vector<std::string> checked_paths;
 
-  return result;
+    const std::string& parameter = node.as<std::string>();
+    const std::string& result = search.find_file(parameter, "", &checked_paths);
+    if (result.empty())
+    {
+        std::string err = std::string()
+                + "[soss::websocket::Server] websocket_server failed to find the "
+                + "specified file for the [" + config_key + "] parameter: [" + parameter
+                + "]. Checked the following paths:\n";
+        for (const std::string& checked_path : checked_paths)
+        {
+            err += " -- " + checked_path + "\n";
+        }
+        std::cerr << err << std::endl;
+    }
+    else
+    {
+        std::cout << "[soss::websocket::Server] For [" << config_key << "] using ["
+                  << result << "]" << std::endl;
+    }
+
+    return result;
 }
 
 //==============================================================================
@@ -128,19 +130,21 @@ static boost::asio::ssl::context::file_format parse_format(
 
 //==============================================================================
 static bool all_tls_closed(
-    const std::unordered_set<TlsConnectionPtr>& connections)
+        const std::unordered_set<TlsConnectionPtr>& connections)
 {
-  for(const auto& connection : connections)
-  {
-    if(connection->get_state() != websocketpp::session::state::closed)
-      return false;
-  }
+    for (const auto& connection : connections)
+    {
+        if (connection->get_state() != websocketpp::session::state::closed)
+        {
+            return false;
+        }
+    }
 
-  return true;
+    return true;
 }
 
 static bool all_tcp_closed(
-    const std::unordered_set<TcpConnectionPtr>& connections)
+        const std::unordered_set<TcpConnectionPtr>& connections)
 {
     for (const auto& connection : connections)
     {
@@ -506,7 +510,6 @@ public:
       }
       _mutex.unlock();
     }
-    _mutex.unlock();
 
     if(_server_thread.joinable())
     {
@@ -578,154 +581,153 @@ public:
 
 private:
 
-  void _handle_tls_message(
-      const ConnectionHandlePtr& handle,
-      const TlsMessagePtr& message)
-  {
-    get_encoding().interpret_websocket_msg(
-          message->get_payload(), *this, _tls_server->get_con_from_hdl(handle));
-  }
+    void _handle_tls_message(
+            const ConnectionHandlePtr& handle,
+            const TlsMessagePtr& message)
+    {
+        get_encoding().interpret_websocket_msg(
+            message->get_payload(), *this, _tls_server->get_con_from_hdl(handle));
+    }
 
     void _handle_tcp_message(
-      const ConnectionHandlePtr& handle,
-      const TcpMessagePtr& message)
-  {
-    get_encoding().interpret_websocket_msg(
-          message->get_payload(), *this, _tcp_server->get_con_from_hdl(handle));
-  }
-
-  void _handle_close(
-      const ConnectionHandlePtr& handle)
-  {
-    if (_use_security)
+            const ConnectionHandlePtr& handle,
+            const TcpMessagePtr& message)
     {
-      const auto connection = _tls_server->get_con_from_hdl(handle);
-      std::cout << "[soss::websocket::Server] closed client connection ["
-                << connection << "]" << std::endl;
-      notify_connection_closed(connection);
-
-      _mutex.lock();
-      _open_tls_connections.erase(connection);
-      _mutex.unlock();
+        get_encoding().interpret_websocket_msg(
+            message->get_payload(), *this, _tcp_server->get_con_from_hdl(handle));
     }
-    else
+
+    void _handle_close(
+            const ConnectionHandlePtr& handle)
     {
-      const auto connection = _tcp_server->get_con_from_hdl(handle);
-      std::cout << "[soss::websocket::Server] closed client connection ["
-                << connection << "]" << std::endl;
-      notify_connection_closed(connection);
+        _mutex.lock();
+        if (_use_security)
+        {
+            const auto connection = _tls_server->get_con_from_hdl(handle);
+            std::cout << "[soss::websocket::Server] closed client connection ["
+                      << connection << "]" << std::endl;
+            notify_connection_closed(connection);
 
-      _mutex.lock();
-      _open_tcp_connections.erase(connection);
-      _mutex.unlock();
+            _open_tls_connections.erase(connection);
+        }
+        else
+        {
+            const auto connection = _tcp_server->get_con_from_hdl(handle);
+            std::cout << "[soss::websocket::Server] closed client connection ["
+                      << connection << "]" << std::endl;
+            notify_connection_closed(connection);
+
+            _open_tcp_connections.erase(connection);
+        }
+        _mutex.unlock();
     }
-  }
 
-  void _handle_opening(
-      const ConnectionHandlePtr& handle)
-  {
-    if (_use_security)
+    void _handle_opening(
+            const ConnectionHandlePtr& handle)
     {
-      const auto connection = _tls_server->get_con_from_hdl(handle);
+        _mutex.lock();
+        if (_use_security)
+        {
+            const auto connection = _tls_server->get_con_from_hdl(handle);
 
-      if(_closing_down)
-      {
-        connection->close(websocketpp::close::status::normal, "shutdown");
-        return;
-      }
+            if (_closing_down)
+            {
+                connection->close(websocketpp::close::status::normal, "shutdown");
+                return;
+            }
 
-      std::cout << "[soss::weboscket::Server] opened connection [" << connection
-                << "]" << std::endl;
-      notify_connection_opened(connection);
-      _mutex.lock();
-      _open_tls_connections.insert(connection);
-      _mutex.unlock();
+            std::cout << "[soss::weboscket::Server] opened connection [" << connection
+                      << "]" << std::endl;
+            notify_connection_opened(connection);
+
+            _open_tls_connections.insert(connection);
+        }
+        else
+        {
+            const auto connection = _tcp_server->get_con_from_hdl(handle);
+
+            if (_closing_down)
+            {
+                connection->close(websocketpp::close::status::normal, "shutdown");
+                return;
+            }
+
+            std::cout << "[soss::weboscket::Server] opened connection [" << connection
+                      << "]" << std::endl;
+            notify_connection_opened(connection);
+
+            _open_tcp_connections.insert(connection);
+        }
+        _mutex.unlock();
     }
-    else
+
+    void _handle_failed_connection(
+            const ConnectionHandlePtr& /*handle*/)
     {
-      const auto connection = _tcp_server->get_con_from_hdl(handle);
-
-      if(_closing_down)
-      {
-        connection->close(websocketpp::close::status::normal, "shutdown");
-        return;
-      }
-
-      std::cout << "[soss::weboscket::Server] opened connection [" << connection
-                << "]" << std::endl;
-      notify_connection_opened(connection);
-
-      _mutex.lock();
-      _open_tcp_connections.insert(connection);
-      _mutex.unlock();
+        std::cout << "[soss::websocket::Server] An incoming client failed to "
+                  << "connect." << std::endl;
     }
-  }
 
-  void _handle_failed_connection(
-      const ConnectionHandlePtr& /*handle*/)
-  {
-    std::cout << "[soss::websocket::Server] An incoming client failed to "
-              << "connect." << std::endl;
-  }
-
-  bool _handle_validate(
-      const ConnectionHandlePtr& handle)
-  {
-    if (!_jwt_validator)
-      return true;
-
-    if (_use_security)
+    bool _handle_validate(
+            const ConnectionHandlePtr& handle)
     {
-      TlsConnectionPtr connection_ptr = _tls_server->get_con_from_hdl(handle);
-      std::vector<std::string> requested_sub_protos = connection_ptr->get_requested_subprotocols();
-      if (requested_sub_protos.size() != 1)
-      {
-        connection_ptr->set_status(websocketpp::http::status_code::unauthorized);
-        return false; // a valid soss client should always send exactly 1 subprotocols.
-      }
+        if (!_jwt_validator)
+        {
+            return true;
+        }
 
-      std::string token = requested_sub_protos[0]; // the subprotocol is the jwt token
-      if (!_jwt_validator->verify(token))
-      {
-        connection_ptr->set_status(websocketpp::http::status_code::unauthorized);
-        return false;
-      }
-      connection_ptr->select_subprotocol(token);
-      return true;
+        if (_use_security)
+        {
+            TlsConnectionPtr connection_ptr = _tls_server->get_con_from_hdl(handle);
+            std::vector<std::string> requested_sub_protos = connection_ptr->get_requested_subprotocols();
+            if (requested_sub_protos.size() != 1)
+            {
+                connection_ptr->set_status(websocketpp::http::status_code::unauthorized);
+                return false; // a valid soss client should always send exactly 1 subprotocols.
+            }
+
+            std::string token = requested_sub_protos[0]; // the subprotocol is the jwt token
+            if (!_jwt_validator->verify(token))
+            {
+                connection_ptr->set_status(websocketpp::http::status_code::unauthorized);
+                return false;
+            }
+            connection_ptr->select_subprotocol(token);
+            return true;
+        }
+        else
+        {
+            TcpConnectionPtr connection_ptr = _tcp_server->get_con_from_hdl(handle);
+            std::vector<std::string> requested_sub_protos = connection_ptr->get_requested_subprotocols();
+            if (requested_sub_protos.size() != 1)
+            {
+                connection_ptr->set_status(websocketpp::http::status_code::unauthorized);
+                return false; // a valid soss client should always send exactly 1 subprotocols.
+            }
+
+            std::string token = requested_sub_protos[0]; // the subprotocol is the jwt token
+            if (!_jwt_validator->verify(token))
+            {
+                connection_ptr->set_status(websocketpp::http::status_code::unauthorized);
+                return false;
+            }
+            connection_ptr->select_subprotocol(token);
+            return true;
+        }
     }
-    else
-    {
-      TcpConnectionPtr connection_ptr = _tcp_server->get_con_from_hdl(handle);
-      std::vector<std::string> requested_sub_protos = connection_ptr->get_requested_subprotocols();
-      if (requested_sub_protos.size() != 1)
-      {
-        connection_ptr->set_status(websocketpp::http::status_code::unauthorized);
-        return false; // a valid soss client should always send exactly 1 subprotocols.
-      }
 
-      std::string token = requested_sub_protos[0]; // the subprotocol is the jwt token
-      if (!_jwt_validator->verify(token))
-      {
-        connection_ptr->set_status(websocketpp::http::status_code::unauthorized);
-        return false;
-      }
-      connection_ptr->select_subprotocol(token);
-      return true;
-    }
-  }
-
-  std::shared_ptr<TlsServer> _tls_server;
-  std::shared_ptr<TcpServer> _tcp_server;
-  bool _use_security;
-  std::thread _server_thread;
-  std::mutex _mutex;
-  EncodingPtr _encoding;
-  SslContextPtr _context;
-  std::unordered_set<TlsConnectionPtr> _open_tls_connections;
-  std::unordered_set<TcpConnectionPtr> _open_tcp_connections;
-  bool _has_spun_once = false;
-  bool _closing_down = false;
-  std::unique_ptr<JwtValidator> _jwt_validator;
+    std::shared_ptr<TlsServer> _tls_server;
+    std::shared_ptr<TcpServer> _tcp_server;
+    bool _use_security;
+    std::thread _server_thread;
+    std::mutex _mutex;
+    EncodingPtr _encoding;
+    SslContextPtr _context;
+    std::unordered_set<TlsConnectionPtr> _open_tls_connections;
+    std::unordered_set<TcpConnectionPtr> _open_tcp_connections;
+    bool _has_spun_once = false;
+    bool _closing_down = false;
+    std::unique_ptr<JwtValidator> _jwt_validator;
 
 };
 
