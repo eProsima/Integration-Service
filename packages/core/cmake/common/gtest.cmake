2661e7733b6b5c2055563624a086625626cad4db
Jose Antonio Moral <joseantoniomoralparras@gmail.com>
2021-04-19 12:58:48 +0200

    Add gtest as ExternalProject only if not found in the system (#70)
    * Add gtest as ExternalProject only if not found in the system
    
    Signed-off-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>
    
    * Fix libgtest compilation. Add boost::array to convert utils templates

--- packages/core/cmake/common/gtest.cmake
+++ packages/core/cmake/common/gtest.cmake
@@ -6,24 +6,34 @@ find_package(Threads REQUIRED)
 
 include(ExternalProject)
 
-ExternalProject_Add(gtest
-    URL https://github.com/google/googletest/archive/master.zip
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
-    INSTALL_COMMAND ""
-    )
+find_package(GTest QUIET)
 
-ExternalProject_Get_Property(gtest source_dir binary_dir)
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
 
-add_library(libgtest IMPORTED STATIC GLOBAL)
-add_dependencies(libgtest gtest)
+if (NOT GTest_FOUND)
 
-set_target_properties(libgtest
-    PROPERTIES
-        "IMPORTED_LOCATION" "${binary_dir}/lib/libgtest.a"
-        "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
-    )
+    ExternalProject_Add(gtest
+        URL https://github.com/google/googletest/archive/master.zip
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
+        INSTALL_COMMAND ""
+        )
 
-include_directories("${source_dir}/googletest/include")
+    ExternalProject_Get_Property(gtest source_dir binary_dir)
+
+    add_library(libgtest IMPORTED STATIC GLOBAL)
+    add_dependencies(libgtest gtest)
+
+    set_target_properties(libgtest
+        PROPERTIES
+            "IMPORTED_LOCATION" "${binary_dir}/lib/libgtest.a"
+            "IMPORTED_LINK_INTERFACE_LIBRARIES" "${CMAKE_THREAD_LIBS_INIT}"
+        )
+
+    include_directories("${source_dir}/googletest/include")
+
+    set(IS_GTEST_EXTERNAL_PROJECT ON)
+
+endif()
 
 macro(add_gtest)
     # Parse arguments
