e7ae0b40880ef2e1455f9d4319b2c4bca67b8747
Jose Antonio Moral <joseantoniomoralparras@gmail.com>
2021-04-19 10:16:50 +0200

    Add gtest as ExternalProject only if not found in the system
    Signed-off-by: Jose Antonio Moral <joseantoniomoralparras@gmail.com>

--- packages/core/cmake/common/gtest.cmake
+++ packages/core/cmake/common/gtest.cmake
@@ -6,13 +6,19 @@ find_package(Threads REQUIRED)
 
 include(ExternalProject)
 
-ExternalProject_Add(gtest
-    URL https://github.com/google/googletest/archive/master.zip
-    PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
-    INSTALL_COMMAND ""
-    )
+find_package(GTest REQUIRED)
 
-ExternalProject_Get_Property(gtest source_dir binary_dir)
+if (NOT GTest_FOUND)
+
+    ExternalProject_Add(gtest
+        URL https://github.com/google/googletest/archive/master.zip
+        PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
+        INSTALL_COMMAND ""
+        )
+
+    ExternalProject_Get_Property(gtest source_dir binary_dir)
+
+endif()
 
 add_library(libgtest IMPORTED STATIC GLOBAL)
 add_dependencies(libgtest gtest)
