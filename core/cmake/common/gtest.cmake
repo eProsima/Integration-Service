92b260d9aee16a8eed978f20612bdf34452beaa7
laura-eprosima <laura@eprosima.com>
2021-07-19 13:22:57 +0200

    Download the last working gtest release version

--- core/cmake/common/gtest.cmake
+++ core/cmake/common/gtest.cmake
@@ -13,7 +13,8 @@ SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -pthread")
 if (NOT GTest_FOUND)
 
     ExternalProject_Add(gtest
-        URL https://github.com/google/googletest/archive/master.zip
+        GIT_REPOSITORY https://github.com/google/googletest.git
+        GIT_TAG release-1.11.0
         PREFIX ${CMAKE_CURRENT_BINARY_DIR}/gtest
         INSTALL_COMMAND ""
         )
